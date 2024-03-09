e03961f19f142732ec3af6013ef680434f4a44a9
Raffian-moin <raffianmoin69@gmail.com>
2024-03-09 21:39:14 +0600

    Persist database storage

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 
 $pdo = new PDO("mysql:host=db;dbname=mydb", 'admin', 'password');
 
-$sql = 'INSERT INTO test_table(name, age, profession) VALUES(:name, :age, :profession)';
+$sql = 'INSERT INTO test_table(name) VALUES(:name)';
 
 $statement = $pdo->prepare($sql);
 
 $statement->execute([
-    ':name' => "Raffian",
-    ':age' => 28,
-    ':profession' => "Machinist",
+    ':name' => "Moin",
 ]);
 
 echo "A row has been successfully inserted"
